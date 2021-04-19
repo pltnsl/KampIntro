bc66c971d92adc171f89dbb12d335ce117339e1c
neslihan pulat <pulat_neslihan@hotmail.com>
2021-04-19 17:31:59 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirme Kampı";
+            string kurs2 = "Programlamaya Giriş Kursu";
+            string kurs3 = "Java";
+            string kurs4 = "Pyhton";
+            string kurs5 = "C++";
+
+
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kampı", "Programlamaya Giriş Kursu", "Java", "C++" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer ");
+        }
+    }
+}
